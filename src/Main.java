c2ac2dc7a26f4965fe25c03249a764742e07a47b
maymottin <may.fernedamottin@gmail.com>
2022-07-09 11:09:17 -0300

    Projeto de cartão de presente.

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ import model.Natal;
 
 public class Main {
     public static void main(String[] args){
-        DiaDasMaes cartaoMaes = new DiaDasMaes("Jesus");
+        DiaDasMaes cartaoMaes = new DiaDasMaes("Jesus", "Maria");
         DiaDosNamorados cartaoNamorados = new DiaDosNamorados("Romeu", "Julieta");
         Natal cartaoNatal = new Natal("Papai Noel","Grinch");
 
-        imprimeCartao(cartaoMaes);
+        mostrarMensagem(cartaoMaes);
         System.out.println();
-        imprimeCartao(cartaoNamorados);
+        mostrarMensagem(cartaoNamorados);
         System.out.println();
-        imprimeCartao(cartaoNatal);
+        mostrarMensagem(cartaoNatal);
     }
 
-    public static void imprimeCartao(CartaoDePresente cartaoDePresente){
+    public static void mostrarMensagem(CartaoDePresente cartaoDePresente){
                 System.out.println(cartaoDePresente.mensagem() +
                 "\nDe: " + cartaoDePresente.getNomeRemetente() +
                 "\nPara: " + cartaoDePresente.getNomeDestinatario());
